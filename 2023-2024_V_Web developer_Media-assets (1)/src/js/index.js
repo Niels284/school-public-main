9995a2709fed6f5330aeb669cc87888d942e8838
Niels284 <40193974@roctilburg.nl>
2023-09-14 14:39:20 +0200

    updated to version 1.1

--- 2023-2024_V_Web developer_Media-assets (1)/src/js/index.js
+++ 2023-2024_V_Web developer_Media-assets (1)/src/js/index.js
@@ -1,67 +1,79 @@
-// insert dataset.json (fetch async)
-function getDataSet(page) {
-  fetch("../dataset.json")
-    .then((response) => response.json())
-    .then((dataset) => {
-      switch (page) {
-        case "shop":
-          getAssortment(dataset);
-          break;
-        case "configuration1":
-          getContent(dataset);
-          getConfiguration1(dataset);
-          break;
-        case "configuration2":
-          getContent(dataset);
-          getConfiguration2(dataset);
-          break;
-        case "succes":
-          getSuccesPage();
-          break;
-        default:
-          console.log(`Sorry, we didn't found the ${page}.`);
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      throw error;
-    });
+let static_dataset = null;
+
+// When the DOM is loaded, this function will run
+$(document).ready(async () => {
+  try {
+    static_dataset = await getDataSet();
+    const current_page = localStorage.getItem("current_page");
+    if (current_page === null) {
+      localStorage.setItem("current_page", "shop");
+    }
+    loadPage(current_page);
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Give buttons an eventlistener
+
+  // gives buttons for configuration 1 page an eventlistener
+  $(".arrow.right.symbol").click(() => {
+    changeId(1, "symbol");
+  });
+  $(".arrow.left.symbol").click(() => {
+    changeId(-1, "symbol");
+  });
+  $("#nextStep").click(() => {
+    changePage("configuration2");
+  });
+  $("#back1, nav > ul > li:nth-child(1)").click(() => {
+    localStorage.removeItem("cart");
+    changePage("shop");
+  });
+
+  // gives buttons for configuration 2 page an eventlistener
+  $(".arrow.right.color").click(() => {
+    changeId(1, "colour");
+  });
+  $(".arrow.left.color").click(() => {
+    changeId(-1, "colour");
+  });
+
+  // gives button an eventlistener
+  $("#order").click(() => {
+    sendDataToServer();
+  });
+  $("#back2").click(() => {
+    changePage("configuration1");
+  });
+  // gives buttons for succes page an eventlistener
+  $("#back3").click(() => {
+    changePage("shop");
+  });
+});
+
+// insert dataset.json to static_dataset (fetch async)
+async function getDataSet() {
+  try {
+    const res = await fetch("../dataset.json");
+    if (!res.ok) {
+      throw new Error("Failed to fetch dataset");
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Sends data to server and gets response
 function sendDataToServer() {
   const cart = JSON.parse(localStorage.getItem("cart"));
 
-  // Creates an object with the data to send the server
-  // If the value is 0, value will be 1 or we get an error from the server
-  let dataToSend = {};
-  if (cart.symbol < 1 && cart.colour >= 1) {
-    dataToSend = {
-      productType: parseInt(cart.id),
-      symbol: 1,
-      colour: parseInt(cart.colour),
-    };
-  } else if (cart.symbol >= 1 && cart.colour < 1) {
-    dataToSend = {
-      productType: parseInt(cart.id),
-      symbol: parseInt(cart.symbol),
-      colour: 1,
-    };
-  } else if (cart.symbol < 1 && cart.colour < 1) {
-    dataToSend = {
-      productType: parseInt(cart.id),
-      symbol: 1,
-      colour: 1,
-    };
-  } else {
-    dataToSend = {
-      productType: parseInt(cart.id),
-      symbol: parseInt(cart.symbol),
-      colour: parseInt(cart.colour),
-    };
-  }
-
-  console.log(dataToSend);
+  let dataToSend = {
+    productType: parseInt(cart.id),
+    symbol: parseInt(cart.symbol),
+    colour: parseInt(cart.colour),
+  };
 
   // Send data in JSON-format to the request body
   fetch("https://skills.canvasaccept.com/orders", {
@@ -79,10 +91,9 @@ function sendDataToServer() {
     })
     .then((data) => {
       if (data.success === true) {
-        console.log("Bestelling succesvol verstuurd");
+        console.log("Bestelling is succesvol geplaatst!");
         localStorage.removeItem("cart");
-        localStorage.setItem("page", "succes");
-        window.location.href = "index.html";
+        changePage("succes");
       } else {
         console.error("Server response: Error - " + data.error);
       }
@@ -92,316 +103,189 @@ function sendDataToServer() {
     });
 }
 
-// When the DOM is loaded, this function will run
-window.addEventListener("DOMContentLoaded", () => {
-  const page = localStorage.getItem("page");
-  switch (page) {
+// CONTENT LOADING FUNCTIONS
+function loadPage(current_page) {
+  $(".active").removeClass("active");
+  $("#back3").removeAttr("style");
+  switch (current_page) {
     case "shop":
-      getDataSet("shop");
+      getAssortment();
       break;
     case "configuration1":
-      getDataSet("configuration1");
+      getDefaultContent();
+      getConfiguration1();
       break;
     case "configuration2":
-      getDataSet("configuration2");
+      getDefaultContent();
+      getConfiguration2();
       break;
     case "succes":
-      getDataSet("succes");
+      getSuccesPage();
       break;
     default:
-      getDataSet("shop");
+      console.error("no page found");
   }
-  document
-    .querySelector("nav > ul > li:nth-child(1)")
-    .addEventListener("click", () => {
-      localStorage.setItem("page", "shop");
-      localStorage.removeItem("cart");
-      window.location.href = "index.html";
-    });
-});
-
-// GENERATES SHOP PAGE
-function getAssortment(dataset) {
-  // defeaul changes at configuration 1 page
-  document.querySelector(".configuration").style.display = "none";
-  document.querySelector("#back3").style.display = "none";
-
-  console.log("Welcome to the shop");
-
-  const assortment = dataset;
-  assortment.products.forEach((products) => {
-    document.querySelector(".products").innerHTML += `
-    <div class="product">
-        <div class="image">
-            <img class="productLayer" data-product="" src="../img/products/${products.name}.png" alt="${products.name}" />
-        </div>
-        <h3>${products.name}</h3>
-        <button data-productid="${products.id}">configurate product</button>
-    </div>
-    `;
-  });
-
-  // Your other code can go here, and it will execute after the async operation is done.
-  const buttons = document.querySelectorAll(".product > button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      // object vullen met product data en omzetten in json
-      const productid = e.target.dataset.productid;
-      let cart = {
-        id: productid,
-        name: dataset.products[productid - 1].name,
-        price: dataset.products[productid - 1].price,
-        totalPrice: dataset.products[productid - 1].price,
-        symbol: "0",
-        colour: "0",
-      };
-      localStorage.setItem("cart", JSON.stringify(cart));
-      localStorage.setItem("page", "configuration1");
-      window.location.href = "index.html";
-    });
-  });
 }
 
 // GETS DEFAULT CONTENT FOR CONFIGURATION PAGES
 
-function getContent(dataset) {
+// GENERATES SHOP PAGE
+function getAssortment() {
+  $(".productContainer").addClass("active");
+  if ($(".products").children().length == 0) {
+    static_dataset.products.forEach((product) => {
+      $(".products").append(`
+      <div class="product">
+          <div class="image">
+              <img class="productLayer" data-product="" src="../img/products/${product.name}.png" alt="${product.name}" />
+          </div>
+          <h3>${product.name}</h3>
+          <button data-productid="${product.id}" class="configurate">configurate product</button>
+      </div>
+      `);
+    });
+    $(".configurate").each(function () {
+      $(this).click(function (e) {
+        // fill object with values
+        const productid = e.target.dataset.productid;
+        let cart = {
+          id: productid,
+          name: static_dataset.products[productid - 1].name,
+          price: static_dataset.products[productid - 1].price,
+          totalPrice: static_dataset.products[productid - 1].price,
+          symbol: "1",
+          colour: "1",
+        };
+        localStorage.setItem("cart", JSON.stringify(cart));
+        changePage("configuration1");
+      });
+    });
+  }
+}
+
+// DEFAULT CONTENT FOR CONFIGURATION PAGES
+function getDefaultContent() {
   // gets cart item from localStorage
   const product = JSON.parse(localStorage.getItem("cart"));
-  let colorName = "";
+  const root = "../img/";
+  const colorObject = getObjectById(static_dataset.colours, product.colour);
+  const symbolObject = getObjectById(static_dataset.symbols, product.symbol);
 
-  // gives product img data/values
-  if (product.colour > 0) {
-    colorName = `-${getObjectById(dataset.colours, product.colour).name}`;
-    document.querySelector("#productColor").innerHTML = `Kleur: ${
-      getObjectById(dataset.colours, product.colour).name
-    }`;
-  } else {
-    colorName = "";
-    document.querySelector("#productColor").innerHTML = `Kleur: wit`;
-  }
+  $(".image").attr("data-productid", product.id);
 
   // product image
-  const productImg = document.querySelector("#productImg");
-  productImg.src = `../img/products/${product.name}${colorName}.png`;
-  productImg.alt = `${product.name}-${colorName}`;
+  $("#productImg").attr(
+    "src",
+    `${root}products/${product.name}-${colorObject.name}.png`
+  );
+  $("#productImg").attr("alt", `${product.name}`);
 
   // product symbol
-  if (product.symbol > 0) {
-    const symbolName = getObjectById(dataset.symbols, product.symbol).name;
-    symbolLayer.src = `../img/symbols/symbol-${symbolName}.png`;
-    symbolLayer.alt = `${symbolName}`;
-    document.querySelector(
-      "#productSymbol"
-    ).innerHTML = `Symbool: ${symbolName}`;
-  } else {
-    symbolLayer.src = ``;
-    symbolLayer.alt = ``;
-    document.querySelector("#productSymbol").innerHTML = `Symbool: leeg`;
-  }
-
-  document.querySelector(".image").dataset.productid = `${product.id}`;
+  $("#symbolLayer").attr(
+    "src",
+    `${root}symbols/symbol-${symbolObject.name}.png`
+  );
+  $("#productSymbol").html(`Symbool: ${symbolObject.name}`);
 
   // product description
-  updateTotalPrice(dataset);
-  document.querySelector("#productName").innerHTML = product.name;
+  updateTotalPrice();
+  $("#productColor").html(`Kleur: ${colorObject.name}`);
+  $("#productName").html(product.name);
 }
 
 // CONFIGURATION PAGE 1
 
-function getConfiguration1(dataset) {
-  console.log("Welcome to the configuration page 1");
-
-  // gets cart item from localStorage
+function getConfiguration1() {
   const product = JSON.parse(localStorage.getItem("cart"));
 
   // defeaul changes at configuration 1 page
-  document.querySelector(".symbolForm").style.display = "block";
-  document.querySelector("#nextStep").style.display = "block";
-  document.querySelector(".colorForm").style.display = "none";
-  document.querySelector("#order").style.display = "none";
-  document.querySelector("#back1").style.display = "block";
-  document.querySelector("#back2").style.display = "none";
-  document.querySelector("#back3").style.display = "none";
+  $(".configurationContainer, .symbolForm, #nextStep, #back1").addClass(
+    "active"
+  );
 
   // gives product symbol data/values (configuration)
-  if (product.symbol > 0) {
-    const symbolName = getObjectById(dataset.symbols, product.symbol).name;
-    symbolIcon.src = `../img/symbols/symbol-${symbolName}.png`;
-    symbolIcon.alt = `${symbolName}`;
-  } else {
-    symbolIcon.src = ``;
-    symbolIcon.alt = ``;
-  }
-
-  // change product symbol by clicking on arrow
-  document
-    .querySelector(".arrow.right.symbol")
-    .addEventListener("click", () => {
-      changeId(1, "symbol", dataset);
-    });
-  document.querySelector(".arrow.left.symbol").addEventListener("click", () => {
-    changeId(-1, "symbol", dataset);
-  });
-
-  // gives button an eventlistener
-  document.querySelector("#nextStep").addEventListener("click", () => {
-    localStorage.setItem("page", "configuration2");
-    window.location.href = "index.html";
-  });
-
-  document.querySelector("#back1").addEventListener("click", () => {
-    localStorage.setItem("page", "shop");
-    localStorage.removeItem("cart");
-    window.location.href = "index.html";
-  });
+  const symbolName = getObjectById(static_dataset.symbols, product.symbol).name;
+  $("#symbolIcon").attr("src", `../img/symbols/symbol-${symbolName}.png`);
+  $("#symbolIcon").attr("alt", `${symbolName}`);
 }
 
 // CONFIGURATION PAGE 2
 
-function getConfiguration2(dataset) {
-  console.log("Welcome to the configuration page 2");
-
+function getConfiguration2() {
   // gets cart item from localStorage
   const product = JSON.parse(localStorage.getItem("cart"));
 
   // defeaul changes at configuration 1 page
-  document.querySelector(".symbolForm").style.display = "none";
-  document.querySelector("#nextStep").style.display = "none";
-  document.querySelector(".colorForm").style.display = "block";
-  document.querySelector("#order").style.display = "block";
-  document.querySelector("#back1").style.display = "none";
-  document.querySelector("#back2").style.display = "block";
-  document.querySelector("#back3").style.display = "none";
+  $(".configurationContainer, .colorForm, #order, #back2").addClass("active");
 
   // gives product color data/values (configuration)
-  const colorIcon = document.querySelector("#colorIcon");
-  if (product.colour > 0) {
-    colorName = getObjectById(dataset.colours, product.colour).name;
-    colorIcon.dataset.color = colorName;
-  } else {
-    colorIcon.dataset.color = 0;
-  }
-
-  // change product color by clicking on arrow (configuration)
-  document.querySelector(".arrow.right.color").addEventListener("click", () => {
-    changeId(1, "colour", dataset);
-  });
-  document.querySelector(".arrow.left.color").addEventListener("click", () => {
-    changeId(-1, "colour", dataset);
-  });
-
-  // gives button an eventlistener
-  document.querySelector("#order").addEventListener("click", () => {
-    document.querySelector(".configuration").style.display = "none";
-    sendDataToServer();
-  });
-
-  document.querySelector("#back2").addEventListener("click", () => {
-    localStorage.setItem("page", "configuration1");
-    window.location.href = "index.html";
-  });
+  $("#colorIcon").attr(
+    "data-color",
+    getObjectById(static_dataset.colours, product.colour).name
+  );
 }
+
+// SUCCES PAGE 2
 
 function getSuccesPage() {
-  const selectedItems = document.querySelectorAll(
-    ".image, #productName, #productPrice, .productSettings, #back1, #nextStep, #back2, #order"
-  );
-  selectedItems.forEach((item) => {
-    item.style.display = "none";
-  });
-
-  document.querySelector("#back3").style.display = "flex";
-
-  document.querySelector("#back3").addEventListener("click", () => {
-    localStorage.setItem("page", "shop");
-    window.location.href = "index.html";
-  });
+  $(".configurationContainer").addClass("active");
+  $("#back3").css("display", "flex");
 }
 
-// FUNCTION / CALCULATIONS
+// FUNCTIONS / CALCULATIONS
+
+// change page
+function changePage(new_page) {
+  localStorage.setItem("current_page", new_page);
+  loadPage(new_page);
+}
 
 // select object by type - value (example: type = symbol/color, productid = 1) and returns the object
 function getObjectById(type, id) {
-  for (let i = 0; i < type.length; i++) {
-    if (type[i].id === id) {
-      return type[i];
+  const elements = type;
+  for (const element of elements) {
+    if (element.id == id) {
+      return element;
     }
   }
-  console.error("no object found");
+  console.error("Geen object gevonden");
   return null;
 }
 
-function changeId(int, type, dataset) {
+// change id of type in localStorage("cart") and reloads dynamicly the page
+function changeId(int, type) {
   let cart = JSON.parse(localStorage.getItem("cart"));
   switch (type) {
     case "symbol":
       cart.symbol = parseInt(cart.symbol) + int;
-      cart.symbol = cart.symbol < 0 ? 9 : cart.symbol;
-      cart.symbol = cart.symbol > 9 ? 0 : cart.symbol;
+      cart.symbol =
+        cart.symbol <= 0 ? static_dataset.symbols.length : cart.symbol;
+      cart.symbol =
+        cart.symbol > static_dataset.symbols.length ? 1 : cart.symbol;
       localStorage.setItem("cart", JSON.stringify(cart));
-      updateTotalPrice(dataset);
-
-      // gives product symbol data/values (configuration)
-      const symbolIcon = document.querySelector("#symbolIcon");
-      const symbolLayer = document.querySelector("#symbolLayer");
-      if (cart.symbol > 0) {
-        const symbolName = getObjectById(dataset.symbols, cart.symbol).name;
-        symbolIcon.src = `../img/symbols/symbol-${symbolName}.png`;
-        symbolIcon.alt = `${symbolName}`;
-        symbolLayer.src = `../img/symbols/symbol-${symbolName}.png`;
-        symbolLayer.alt = `${symbolName}`;
-        document.querySelector(
-          "#productSymbol"
-        ).innerHTML = `Symbool: ${symbolName}`;
-      } else {
-        symbolIcon.src = ``;
-        symbolIcon.alt = ``;
-        symbolLayer.src = ``;
-        symbolLayer.alt = ``;
-        document.querySelector("#productSymbol").innerHTML = `Symbool: leeg`;
-      }
+      loadPage("configuration1");
       break;
     case "colour":
       cart.colour = parseInt(cart.colour) + int;
-      cart.colour = cart.colour < 0 ? 4 : cart.colour;
-      cart.colour = cart.colour > 4 ? 0 : cart.colour;
+      cart.colour =
+        cart.colour <= 0 ? static_dataset.colours.length : cart.colour;
+      cart.colour =
+        cart.colour > static_dataset.colours.length ? 1 : cart.colour;
       localStorage.setItem("cart", JSON.stringify(cart));
-      updateTotalPrice(dataset);
-
-      let colorName = "";
-
-      // gives product color data/values (configuration)
-      const colorIcon = document.querySelector("#colorIcon");
-      if (cart.colour > 0) {
-        colorName = getObjectById(dataset.colours, cart.colour).name;
-        colorIcon.dataset.color = colorName;
-
-        colorName = `-${getObjectById(dataset.colours, cart.colour).name}`;
-        document.querySelector("#productColor").innerHTML = `Kleur: ${
-          getObjectById(dataset.colours, cart.colour).name
-        }`;
-      } else {
-        colorIcon.dataset.color = 0;
-        colorName = "";
-        document.querySelector("#productColor").innerHTML = `Kleur: wit`;
-      }
-
-      // product image
-      const productImg = document.querySelector("#productImg");
-      productImg.src = `../img/products/${cart.name}${colorName}.png`;
-      productImg.alt = `${cart.name}-${colorName}`;
+      loadPage("configuration2");
       break;
     default:
       console.log("no type found");
   }
 }
 
-function updateTotalPrice(dataset) {
+// this function will automatically update the totalprice
+function updateTotalPrice() {
   const cart = JSON.parse(localStorage.getItem("cart"));
   const productPrice = cart.price;
   const colorPrice =
     cart.colour > 0
-      ? parseInt(getObjectById(dataset.colours, cart.colour).price_add)
+      ? parseInt(getObjectById(static_dataset.colours, cart.colour).price_add)
       : 0;
   const totalPrice = (
     Math.round((productPrice + colorPrice) * 100) / 100
@@ -409,7 +293,5 @@ function updateTotalPrice(dataset) {
 
   cart.totalPrice = totalPrice;
   localStorage.setItem("cart", JSON.stringify(cart));
-  document.querySelector(
-    "#productPrice"
-  ).innerHTML = `Totaalprijs €${totalPrice}`;
+  $("#productPrice").html(`Totaalprijs €${totalPrice}`);
 }
